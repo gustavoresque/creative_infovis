6bf9cb45fa44789202ab72c5baf565cc68674e47
elvis thermo <elvisthermo@hotmail.com>
2017-11-13 14:32:51 -0200

    menu

---
+++ client/libs/main.js
@@ -0,0 +1,19 @@
+
+/*
+$(function () {
+    $(".pure-menu-list").menu();
+
+    });
+
+
+var mysocket = new AppSocket();
+
+mysocket.on("open", function(){
+    console.log("deu certo");
+    mysocket.send("tools", "olá")
+});
+
+*/
+
+
+
