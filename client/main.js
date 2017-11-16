9cddf1e74f5744e7db5288b6e7689d3ef76f3e71
Rodrigo Lima <rodrigo.sad.lima@gmail.com>
2017-11-16 13:12:22 -0300

    Implementada conexão via chave
    Conexão por chave implementada

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-let connection;
+let key;
 const mysocket = new AppSocket();
 
 mysocket.on("open", function(){
@@ -6,12 +6,12 @@ mysocket.on("open", function(){
     mysocket.send("tools", "olá")
 });
 
-mysocket.on("connect", (c) => {
-    console.log(c);
-    connection = c;
+mysocket.on("connect", (res) => {
+    console.log('oq o cliente recebe:', res.connection_key);
+    key = res.connection_key;
 });
-mysocket.on('metadata', (m) => {
-    //console.log(m);
+mysocket.on('metadata', (res) => {
+    console.log(res.result);
 });
 
 
@@ -25,10 +25,10 @@ $(document).ready(function(){
         console.log(a,b);
     };
     $("#connect").click(()=>{
-        mysocket.send('connect', {base_path: '../server/test_unity/sqlite_files/tests.sqlite'});
+        mysocket.send('connect', {base_name: 'tests.sqlite'});
     });
     $("#meta").click(()=>{
-        mysocket.send('metadata', {connection: connection, table_name:"imaginary_sells"});
+        mysocket.send('metadata', {connection_key: key, table_name:"imaginary_sells"});
     });
 });
 
